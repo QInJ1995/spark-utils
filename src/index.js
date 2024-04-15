06201af6c19b413e9695a3ebf3e9fd07bdb8929f
QINJIN <953373752@qq.com>
2024-04-15 16:15:19 +0800

    fix: 项目结构优化

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@
  * @Author: QINJIN
  * @Date: 2024-02-26 20:01:50
  * @LastEditors: QINJIN
- * @LastEditTime: 2024-04-15 16:01:53
+ * @LastEditTime: 2024-04-15 16:14:03
  * @FilePath: /spark-utils/src/index.js
  * @Description: index.js
  * Copyright (c) 2024 by QINJIN, All Rights Reserved. 
  */
 
 // basic
-import basic from './basic'
+import basicMethods from './basic'
 // array
 import arrayMethods from './array'
+// date
+import dateMethods from './date'
+// data
+import dataMethods from './data'
 // function 
 import functionMethods from './function'
 // object
@@ -20,8 +24,10 @@ import objectMethods from './object'
 import otherMethods from './other'
 
 const SparkUtils = {
-    ...basic,
+    ...basicMethods,
     ...arrayMethods,
+    ...dateMethods,
+    ...dataMethods,
     ...functionMethods,
     ...objectMethods,
     ...otherMethods
