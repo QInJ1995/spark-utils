5861eaf9d5c4ad4ed82f39a52330a165db16998b
QINJIN <953373752@qq.com>
2024-04-17 17:06:59 +0800

    feat: 新增方法

--- src/function/index.js
+++ src/function/index.js
@@ -2,16 +2,28 @@
  * @Author: QINJIN
  * @Date: 2024-04-15 15:52:38
  * @LastEditors: QINJIN
- * @LastEditTime: 2024-04-15 16:00:55
+ * @LastEditTime: 2024-04-17 17:06:11
  * @FilePath: /spark-utils/src/function/index.js
  * @Description: 封装方法聚合
  * Copyright (c) 2024 by QINJIN, All Rights Reserved. 
  */
 
-import debounce  from './debounce.js'
+import noop from './noop'
+import delay from './delay'
+import bind from './bind'
+import once from './once'
+import after from './after'
+import before from './before'
 import throttle  from './throttle.js'
+import debounce  from './debounce.js'
 
 export default {
-    debounce,
-    throttle
+    noop,
+    delay,
+    bind,
+    once,
+    after,
+    before,
+    throttle,
+    debounce
 }
