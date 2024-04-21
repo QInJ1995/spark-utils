382ff06f89baabfb2374e3019986673b76321e62
QINJIN <953373752@qq.com>
2024-04-21 21:40:12 +0800

    feat: 新增打包进度显示

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const WebpackBar = require('webpackbar')
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin') // 引入压缩插件
 
 module.exports = {
     mode: 'none', // 因为默认是production 默认会进行压缩
@@ -34,6 +35,23 @@ module.exports = {
         ]
     },
     plugins: [
+        new WebpackBar({
+            // color: "#85d",  // 默认green，进度条颜色支持HEX
+            basic: false,   // 默认true，启用一个简单的日志报告器
+            profile: false,  // 默认false，启用探查器。
+            reporter: {
+                start(context) {
+                    console.log(
+" ____                   _      _   _ _   _ _       _ " + '\n' + 
+"/ ___| _ __   __ _ _ __| | __ | | | | |_(_) |___  | |" + '\n' +
+"\\___ \\| '_ \\ / _` | '__| |/ / | | | | __| | / __| | |" + '\n' +
+" ___) | |_) | (_| | |  |   <  | |_| | |_| | \\__ \\ |_|" + '\n' +
+"|____/| .__/ \\__,_|_|  |_|\\_\\  \\___/ \\__|_|_|___/ (_)" + '\n' +
+"      |_|                                            " + '\n' 
+                    )
+                }, 
+            },
+          }),
     //     new HtmlWebpackPlugin({
     //     // 以 public/index.html 为模板创建文件
     //     // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
