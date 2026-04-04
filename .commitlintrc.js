4f18c03f3231bd8c469e6e3e74c18a419c2fdc54
QINJIN <953373752@qq.com>
2026-04-04 23:26:26 +0800

    style: 代码规范

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -16,22 +16,17 @@ module.exports = {
 			2,
 			'always',
 			[
-				'feat', // 增加一个新特性
-				'fix', //  修复一个 bug
-				'perf', // 更改代码以提高性能
-				'style', // 不影响代码含义的改动，例如去掉空格、改变缩进、增删分号
-				'docs', // 仅仅修改文档说明
-				'test', // 增加新的测试功能或更改原有的测试模块
-				'refactor', // 代码重构时使用
-				'build', // 更改构建系统和外部依赖项（如将 gulp 改为 webpack，更新某个 npm 包）
-				'ci', // 对 CI 配置文件和脚本的更改
-				'chore', // 其他繁杂事务的变动
-				'revert', // 代码回滚（体现在 changelog）
-				'wip', // 添加图例展示，格式化x轴label
-				'workflow', // 工作流
-				'types', // 添加类型定义
-				'release', // 发布版本
-				'temp' // 临时的提交
+				'feat', // ✨ 新功能
+				'fix', // 🐛 修复bug
+				'docs', // 📝 文档变更
+				'style', // 💄 代码格式（不影响功能）
+				'refactor', // ♻️ 重构（非bug/非功能）
+				'perf', // ⚡️ 性能优化
+				'test', // ✅ 测试用例
+				'build', // 👷 构建/依赖变更
+				'ci', // 🔧 CI/CD配置
+				'chore', // 🎨 其他（构建/工具/脚本）
+				'revert' // ⏪ 回滚提交
 			]
 		]
 	}
